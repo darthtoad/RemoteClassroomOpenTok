77523798844d0914462f4b057a8275e809a237bc
Sam Gespass <samuel.gespass@gmail.com>
2018-04-09 14:42:22 -0700

    initial commit. add basic opentok functionality

---
+++ app/src/main/java/com/epicodus/samuelgespass/remoteclassroomopentok/Constants.java
@@ -0,0 +1,11 @@
+package com.epicodus.samuelgespass.remoteclassroomopentok;
+
+/**
+ * Created by Guest on 4/9/18.
+ */
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.ApiKey;
+    public static final String SESSION_ID = BuildConfig.SessionId;
+    public static final String TOKEN = BuildConfig.Token;
+}
---
+++ app/src/main/java/com/epicodus/samuelgespass/remoteclassroomopentok/MainActivity.java
@@ -0,0 +1,177 @@
+package com.epicodus.samuelgespass.remoteclassroomopentok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.opentok.android.Session;
+import com.opentok.android.Stream;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Subscriber;
+import com.opentok.android.OpentokError;
+import android.support.annotation.NonNull;
+import android.Manifest;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements Session.SessionListener, PublisherKit.PublisherListener, View.OnClickListener {
+
+    private static String API_KEY = Constants.API_KEY;
+    private static String SESSION_ID = Constants.SESSION_ID;
+    private static String TOKEN = Constants.TOKEN;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SETTINGS_SCREEN_PERM = 123;
+    private static final int RC_VIDEO_APP_PERM = 124;
+    private Session mSession;
+    private FrameLayout mPublisherViewContainer;
+    private FrameLayout mSubscriberViewContainer;
+    private Publisher mPublisher;
+    private Subscriber mSubscriber;
+    private Button mFlipScreen;
+
+    public void fetchSessionConnectionData() {
+        RequestQueue reqQueue = Volley.newRequestQueue(this);
+        reqQueue.add(new JsonObjectRequest(Request.Method.GET,
+                "https://remote-classroom-open-tok.herokuapp.com" + "/session",
+                null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    API_KEY = response.getString("apiKey");
+                    SESSION_ID = response.getString("sessionId");
+                    TOKEN = response.getString("token");
+
+                    Log.i(LOG_TAG, "API_KEY: " + API_KEY);
+                    Log.i(LOG_TAG, "SESSION_ID: " + SESSION_ID);
+                    Log.i(LOG_TAG, "TOKEN: " + TOKEN);
+
+                    mSession = new Session.Builder(MainActivity.this, API_KEY, SESSION_ID).build();
+                    mSession.setSessionListener(MainActivity.this);
+                    mSession.connect(TOKEN);
+
+                } catch (JSONException error) {
+                    Log.e(LOG_TAG, "Web Service error: " + error.getMessage());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(LOG_TAG, "Web Service error: " + error.getMessage());
+            }
+        }));
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestPermissions();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @AfterPermissionGranted(RC_VIDEO_APP_PERM)
+    private void requestPermissions() {
+
+        String[] perms = { Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            mPublisherViewContainer = (FrameLayout)findViewById(R.id.publisher_container);
+            mSubscriberViewContainer = (FrameLayout)findViewById(R.id.subscriber_container);
+            mFlipScreen = (Button) findViewById(R.id.button_toggle_screen);
+
+            mSession = new Session.Builder(this, API_KEY, SESSION_ID).build();
+            mSession.setSessionListener(this);
+            mSession.connect(TOKEN);
+
+
+        } else {
+            EasyPermissions.requestPermissions(this, "This app needs access to your camera and mic to make video calls", RC_VIDEO_APP_PERM, perms);
+        }
+    }
+
+    @Override
+    public void onConnected(Session session) {
+        Log.i(LOG_TAG, "Session Connected");
+
+        mPublisher = new Publisher.Builder(this).build();
+        mPublisher.setPublisherListener(this);
+
+        mPublisherViewContainer.addView(mPublisher.getView());
+        mSession.publish(mPublisher);
+    }
+
+    @Override
+    public void onDisconnected(Session session) {
+        Log.i(LOG_TAG, "Session Disconnected");
+    }
+
+    @Override
+    public void onStreamReceived(Session session, Stream stream) {
+        Log.i(LOG_TAG, "Stream Received");
+        if (mSubscriber == null) {
+            mSubscriber = new Subscriber.Builder(this, stream).build();
+            mSession.subscribe(mSubscriber);
+            mSubscriberViewContainer.addView(mSubscriber.getView());
+        }
+    }
+
+    @Override
+    public void onStreamDropped(Session session, Stream stream) {
+        Log.i(LOG_TAG, "Stream Dropped");
+
+        if (mSubscriber != null) {
+            mSubscriber = null;
+            mSubscriberViewContainer.removeAllViews();
+        }
+    }
+
+    @Override
+    public void onStreamCreated(PublisherKit publisherKit, Stream stream) {
+        Log.i(LOG_TAG, "Publisher onStreamCreated");
+    }
+
+    @Override
+    public void onStreamDestroyed(PublisherKit publisherKit, Stream stream) {
+        Log.i(LOG_TAG, "Publisher onStreamDestroyed");
+    }
+
+    @Override
+    public void onError(PublisherKit publisherKit, OpentokError opentokError) {
+        Log.e(LOG_TAG, "Publisher error: " + opentokError.getMessage());
+    }
+
+    @Override
+    public void onError(Session session, OpentokError opentokError) {
+        Log.e(LOG_TAG, "Session error: " + opentokError.getMessage());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mFlipScreen) {
+            mPublisher.swapCamera();
+        }
+    }
+
+}
+// 101e71c5ab49051565679d649968d0fd2f08c155
